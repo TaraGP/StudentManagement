6ad30462ad127c32af541ec4ffa4031a8791bb4c
Tara G P <tara.enner@gmail.com>
2023-03-23 11:24:25 +0530

    Add project files.

---
+++ StudentManagement/Models/StudentStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagement.Models
+{
+    public class StudentStoreDatabaseSettings
+    {
+        public string StudentCoursesCollectionName { get; set; } = string.Empty;
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
---
+++ StudentManagement/Services/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using StudentManagement.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
+namespace StudentManagement.Services
+{
+    public class StudentService 
+    {
+        private readonly IMongoCollection<Student> _students;
+        /*public StudentService(IStudentStoreDatabaseSettings settings, IMongoClient mongoClient) 
+        {
+            var database=mongoClient.GetDatabase(settings.DatabaseName);
+            _students = database.GetCollection<Student>(settings.StudentCoursesCollectionName);
+
+        }*/
+        public StudentService(IOptions<StudentStoreDatabaseSettings> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString); _students = mongoClient.GetDatabase(options.Value.DatabaseName)
+            .GetCollection<Student>(options.Value.StudentCoursesCollectionName);
+        }
+
+        public Student Create(Student student)
+        {
+            _students.InsertOne(student);
+            return student;
+        }
+
+        public List<Student> Get()
+        {
+            return _students.Find(student => true).ToList();
+        }
+
+        public Student Get(string id)
+        {
+            return _students.Find(student => student.Id==id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+             _students.DeleteOne(student => student.Id == id);
+        }
+
+        public void Update(string id, Student student)
+        {
+            _students.ReplaceOne(student => student.Id == id,student);
+        }
+    }
+}
